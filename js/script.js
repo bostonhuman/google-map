30bc58ef8fc2b9d0b0b6491ba274b09bff315d62
Edward Jornes <sethyathet@gmail.com>
2016-06-09 22:30:02 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,11 +6,19 @@ function initMap() {
      zoom: 8,
      mapTypeId: google.maps.MapTypeId.ROADMAP
      });
-     //Create marker
+     // Create infoWindow
+     var contentString = '<h2>Hello, Katelyn</h2>';
+     var infowindow = new google.maps.InfoWindow({
+          content: contentString
+     });
+     // Create marker
      var marker = new google.maps.Marker({
     	position: {lat: 42.34852, lng: -71.08230},
     	map: map,
     	title: 'Hello World!'
      });
+     marker.addListener('click', function() {
+          infowindow.open(map, marker);
+      });
 }
 
