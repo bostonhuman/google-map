34821e7cbc2eb1cf36622b5f360c11cfeb634cac
Edward Jornes <sethyathet@sethyathets-MacBook-Air.local>
2016-06-09 21:41:54 -0400

    js

--- js/script.js
+++ js/script.js
@@ -3,6 +3,13 @@ var map;
 function initMap() {
      map = new google.maps.Map(document.getElementById('map'), {
      center: {lat: -34.397, lng: 150.644},
-     zoom: 8
+     zoom: 8,
+     mapTypeId: google.maps.MapTypeId.ROADMAP
      });
+     //Create marker
+     var markerOptions = {
+	     position: new google.maps.LatLng(-34.397, 150.644)
+     };
+     var marker = new google.maps.Marker(markerOptions);
+     marker.setMap(map);
 }
